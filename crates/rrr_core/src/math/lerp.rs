41a1d228485e2e3d4464dbb4db18a4fe386a0aa2
Zageron <hello@zageron.com>
2022-10-02 14:55:01 -0700

    feat(core): add initial rrr_core crate

---
+++ crates/rrr_core/src/math/lerp.rs
@@ -0,0 +1,25 @@
+use num_traits::Float;
+use std::ops::{Add, Mul, Sub};
+
+pub trait Lerp<F> {
+    #[must_use]
+    fn lerp(self, other: Self, t: F) -> Self;
+
+    #[must_use]
+    fn inv_lerp(self, other: Self, v: Self) -> F;
+}
+
+impl<T, F> Lerp<F> for T
+where
+    T: Add<Output = T> + Mul<F, Output = T> + Sub<Output = F>,
+    F: Float,
+    Self: Copy,
+{
+    fn lerp(self, other: T, t: F) -> T {
+        self * (F::one() - t) + other * t
+    }
+
+    fn inv_lerp(self, other: T, v: T) -> F {
+        (v - self) / (other - self)
+    }
+}
