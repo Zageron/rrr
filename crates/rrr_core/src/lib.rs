ae27c662a137c997cac3ea54aa428fb2a0618a88
Zageron <hello@zageron.ca>
2022-10-02 14:55:01 -0700

    feat(core): add initial rrr_core crate

---
+++ crates/rrr_core/src/lib.rs
@@ -0,0 +1,73 @@
+#![deny(rust_2018_idioms)]
+#![warn(
+    elided_lifetimes_in_paths,
+    trivial_casts,
+    trivial_numeric_casts,
+    unreachable_pub,
+    variant_size_differences,
+    clippy::all,
+    clippy::cargo,
+    clippy::pedantic,
+    clippy::as_conversions,
+    clippy::clone_on_ref_ptr,
+    clippy::dbg_macro,
+    clippy::decimal_literal_representation,
+    clippy::exit,
+    clippy::expect_used,
+    clippy::filetype_is_file,
+    clippy::float_cmp_const,
+    clippy::get_unwrap,
+    clippy::indexing_slicing,
+    clippy::integer_arithmetic,
+    clippy::integer_division,
+    clippy::let_underscore_must_use,
+    clippy::lossy_float_literal,
+    clippy::mem_forget,
+    clippy::multiple_inherent_impl,
+    clippy::panic,
+    clippy::pattern_type_mismatch,
+    clippy::print_stdout,
+    clippy::rest_pat_in_fully_bound_structs,
+    clippy::shadow_reuse,
+    clippy::todo,
+    clippy::unimplemented,
+    clippy::unneeded_field_pattern,
+    clippy::unreachable,
+    clippy::unwrap_used,
+    clippy::use_debug,
+    clippy::verbose_file_reads
+)]
+#![allow(clippy::module_name_repetitions, clippy::multiple_crate_versions)]
+#![forbid(unsafe_code)]
+
+use rrr_settings_core::CoreSettings;
+
+#[derive(Debug, Default)]
+pub struct RRR {
+    _settings: CoreSettings,
+}
+
+impl RRR {
+    #[must_use]
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    #[must_use]
+    pub fn with_settings(settings: CoreSettings) -> Self {
+        Self {
+            _settings: settings,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn custom_settings() {
+        let settings = CoreSettings::default();
+        let _rrr = RRR::with_settings(settings);
+    }
+}
