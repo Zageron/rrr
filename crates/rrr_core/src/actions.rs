72bdc55acb69f115989194d65ac6262ed4669920
Zageron <hello@zageron.ca>
2022-10-02 20:33:40 -0700

    feat(core): add existing core crate, heavily modified

---
+++ crates/rrr_core/src/actions.rs
@@ -0,0 +1,22 @@
+use rrr_chart::RuntimeNote;
+use std::fmt::Display;
+
+#[derive(Debug, Clone)]
+pub enum ActionState {
+    Miss,
+    Boo,
+    Hit,
+}
+
+#[derive(Debug, Clone)]
+pub struct NoteAction {
+    pub note: RuntimeNote,
+    pub timestamp: i128,
+    pub state: ActionState,
+}
+
+impl Display for NoteAction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.note)
+    }
+}
---
+++ crates/rrr_core/src/record.rs
@@ -0,0 +1,35 @@
+use btreemultimap::BTreeMultiMap;
+use rrr_chart::{RuntimeChart, RuntimeNote};
+
+#[derive(Debug, Clone)]
+pub struct Record {
+    pub optimized_chart: BTreeMultiMap<u32, RuntimeNote>,
+    pub mp3: Vec<u8>,
+    pub chart: RuntimeChart,
+    pub duration: u32,
+}
+
+impl Record {
+    /// # Errors
+    /// If duration of the chart is invalid, returns an error.
+    pub fn new(mp3: Vec<u8>, chart: RuntimeChart) -> Result<Self, anyhow::Error> {
+        if let Ok(duration) = chart.get_duration() {
+            Ok(Self {
+                optimized_chart: create_optimized_chart(&chart),
+                mp3,
+                chart,
+                duration,
+            })
+        } else {
+            Err(anyhow::anyhow!("Invalid chart of unknown length."))
+        }
+    }
+}
+
+fn create_optimized_chart(chart: &RuntimeChart) -> BTreeMultiMap<u32, RuntimeNote> {
+    let mut optimized_chart = BTreeMultiMap::new();
+    for note in &chart.notes {
+        optimized_chart.insert(note.timestamp, note.clone());
+    }
+    optimized_chart
+}
