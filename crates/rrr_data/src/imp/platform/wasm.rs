683244baec368fd61bf191ab65ce93e2364c4cf5
Zageron <hello@zageron.ca>
2022-10-03 22:35:55 -0700

    fix(rrr_dat): remove warnings

--- crates/rrr_data/src/imp/platform/wasm.rs
+++ crates/rrr_data/src/imp/platform/wasm.rs
@@ -2,14 +2,14 @@ use serde::{Deserialize, Serialize};
 
 use uuid::Uuid;
 
-pub fn write<'a, T>(id: Uuid, data: T) -> bool
+pub fn write<'a, T>(_id: Uuid, _data: T) -> bool
 where
     T: Serialize + Deserialize<'a>,
 {
     false
 }
 
-pub fn read<'a, T>(id: Uuid, data: T) -> bool
+pub fn read<'a, T>(_id: Uuid, _data: T) -> bool
 where
     T: Serialize + Deserialize<'a>,
 {
